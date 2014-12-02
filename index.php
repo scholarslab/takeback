67b0c41bd61d109486eee53f1bf32c548a4a9f0d
Jeremy Boggs <jeremy@clioweb.org>
2014-12-02 13:23:45 -0500

    Updates text on home page.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
-<p>Take Back The Archive is a collection of documents, images, artifacts, and ephemeral data related to the history of sexual violence at the University of Virginia. It was created as a response to the "we didn't realize" and "we just didn't know" statements made in the aftermath of the Rolling Stone article published on November 19, 2014 about rape at UVa. The goal of this crowd-sourced archive is to visualize documentation and preserve survivors stories related to sexual assault at UVa.</p>
+<p>On November 19, 2014, the University of Virginia community was shocked by revelations in a <em>Rolling Stone</em> magazine article about sexual assault at UVa: <a href="http://www.rollingstone.com/culture/features/a-rape-on-campus-20141119">“A Rape on Campus.”</a> Should we have been shocked?</p>
+
+<p>“Take Back the Archive” is a public history project created by UVa faculty, students, librarians, and archivists in response to all of the “we didn’t realize” and “we just didn’t know” statements made in the aftermath of the <em>Rolling Stone</em> publication. It is meant to preserve, visualize, and contextualize the history of sexual violence at the University of Virginia.</p>
+
+<p>We will soon be soliciting crowd-sourced material: documents, images, artifacts, ephemera, and survivor stories. Stay tuned for information about how you can contribute. Please tweet about the project using the hashtag <a href="https://twitter.com/search?q=%23takebackthearchive">#TakeBackTheArchive</a> and <a href="mailto:XXXXXX">contact us</a> if you have questions about this work. We hope our use of <a href="http://omeka.org">Omeka</a> and <a href="http://neatline.org">Neatline</a> can help establish a national model for college and university communities wishing to memorialize, historicize, confront, and end sexual violence.</p>
 
 <?php fire_plugin_hook('public_append_to_home', array('view' => $this)); ?>
 
