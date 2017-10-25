62b74e6d6452012f8ad68810446a3648749a3fee
Katherine <kbd3ax@virginia.edu>
2017-10-25 14:33:10 -0400

    Add dummy text to divs.

--- collections/show-test/print-divs.py
+++ collections/show-test/print-divs.py
@@ -2,6 +2,6 @@
 
 def printDivs(num):
 	for i in range(num):
-		print('<div class="item">Item ' + str(i+1) + '</div>')
+		print('<div class="item">Item ' + str(i+1) + ': Lorem ipsum dolor sic amet</div>')
 
 printDivs(20)
