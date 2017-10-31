8ec56a8bd805ea0c816514399ea41d0fad2c0c16
Katherine <kbd3ax@virginia.edu>
2017-10-31 11:56:54 -0400

    Change structure of collections/show.php
    -Transfer structure of coll-show-layout.html to show.php
    
    -Notes:
      1. Classes present in show.php have not yet been transferred to main
         style.scss/style.css
      2. Looping through items twice seems inefficient. Need to display
         date range before access items for gallery, but feel as though
         there's better way to do it...?

--- collections/show.php
+++ collections/show.php
@@ -7,50 +7,76 @@ $myItems = get_records('Item', ['collection_id' => $myId], $totalItems); // 10/2
 $years = [];
 ?>
 
-    <article class="collection" id="collection-<?php echo metadata('collection', 'id'); ?>">
-<header>
-<h1><?php echo $collectionTitle; ?></h1>
-<?php if ($totalItems > 0): ?>
-<em class="total-records"><?php echo $totalItems; ?> Items</em>
-<?php endif; ?>
-<div class="collection-metadata">
-<?php if ($description = metadata('collection', array('Dublin Core', 'Description'))): ?>
-<div class="description"><?php echo $description; ?></div>
-<?php endif; ?>
-
-</div>
-</header>
-<div class="content">
-<div class="gallery">
+<!-- Calculate date range: -->
 <?php foreach (loop('items', $myItems) as $item): ?>
-<?php $itemTitle = metadata('item', 'display_title'); // NB: this is setting var, not printing anything ?> 
-<div class="item record hentry">
-    <?php if (metadata('item', 'has files')): ?>
-    <div class="item-img">
-    	<span><?php echo __('File Preview:'); ?></span>
-        <?php echo link_to_item(item_image('thumbnail')); ?>
-    </div>
-    <?php endif; ?>
-
-    <span><?php echo __('Title:'); ?></span>
-    <?php echo link_to_item($itemTitle, array('class'=>'permalink')); ?>
-
-    <?php if($date = check_date_string(metadata('item', array('Dublin Core', 'Date')))): ?>
-    <span><?php echo __('Date:'); ?></span>
-	<div class="eyebrow dc-date date"><?php echo date('F j, Y', $date); ?></div>
-	<?php $years[] = date('Y', $date); ?>
-	<?php endif; ?>
-
-</div>
+<?php if($date = check_date_string(metadata('item', array('Dublin Core', 'Date')))): ?>
+    <?php $years[] = date('Y', $date); ?>
+<?php endif; ?>
 <?php endforeach; ?>
 
-<div style="border:1px solid orange;">
-	<?php echo min($years) . ' to ' . max($years);
-	// NB^: code to display year range. ?>
-</div>
+<article class="collection" id="collection-<?php echo metadata('collection', 'id'); ?>">
+<!-- Landing page -->
+<header>
+    <div class="small-diagonal"></div>
+    <div class="big-diagonal"></div>
+    <div class="title">
+        <span>COLLECTION</span><br/>
+        <h1><?php echo $collectionTitle; ?></h1>
+    </div>
+</header>
 
-</div>
-</div>
+<div class="content">
+
+    <div class="line-wrapper">
+        <div class="item-wrapper">
+            <div class="vertical-line"></div>
+        </div>
+        <!-- Summary statistics: -->
+        <div class="collection-stats">
+            <div>Summary Statistics</div>
+            <?php if ($totalItems > 0): ?>
+            <span>Number of Records:</span> <?php echo $totalItems; ?><br/>
+            <?php endif; ?>
+            <span>Year Range:</span> <?php echo min($years) . ' - ' . max($years); ?><br/> <!-- LOOP -->
+            <span>Collection ID:</span> <?php echo $myId; ?>
+        </div>
+    </div> <!-- end ".line-wrapper" -->
+    
+    <!-- Description: -->
+    <?php if ($description = metadata('collection', array('Dublin Core', 'Description'))): ?>
+    <div class="description"><span>DESCRIPTION</span><br/><?php echo $description; ?></div>
+    <?php endif; ?>
+
+    <div class="the-collection">Gallery</div>
+
+    <div class="gallery-bg">
+    <div class="item-container">
+    <!-- Loop through items: -->
+    <?php foreach (loop('items', $myItems) as $item): ?>
+    <?php $itemTitle = metadata('item', 'display_title'); // NB: this is setting var, not printing anything ?> 
+    <!-- Display files, title, and date for each item: -->
+    <div class="item record hentry">
+        <!-- Files -->
+        <?php if (metadata('item', 'has files')): ?>
+        <div class="item-img">
+    	   <span><?php echo __('File Preview:'); ?></span>
+            <?php echo link_to_item(item_image('thumbnail')); ?>
+        </div>
+        <?php endif; ?>
+        <!-- Title -->
+        <span><?php echo __('Title:'); ?></span>
+        <?php echo link_to_item($itemTitle, array('class'=>'permalink')); ?>
+        <!-- Date -->
+        <?php if($date = check_date_string(metadata('item', array('Dublin Core', 'Date')))): ?>
+        <span><?php echo __('Date:'); ?></span>
+	   <div class="eyebrow dc-date date"><?php echo date('F j, Y', $date); ?></div>
+	   <?php $years[] = date('Y', $date); ?>
+	   <?php endif; ?>
+    </div>
+    <?php endforeach; ?>
+    <!-- End loop -->
+    </div>
+    </div>
 
 <?php fire_plugin_hook('public_collections_show', array('view' => $this, 'collection' => $collection)); ?>
 </article>
