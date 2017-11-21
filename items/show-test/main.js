f9b9d88fe3f80b4ab2988588252d534cd5d736e5
Katherine <kbd3ax@virginia.edu>
2017-11-21 15:22:00 -0500

    Add code to make clickable link area = img area
    -Issue: Item image is wrapped in link that takes you to its full-size
            PDF. By default, CSS renders link as maximum possible width/
            height of the image *regardless of image displayed size*. That
            means there's lots of whitespace around clickable images that
            users don't realize is clickable -> accidentally click off of
            page -> bad experience.
    
    -Explanations:
    1.) main.js
      a. Solution: add JS to grab the actual height of the image and make
         the <a> tag same height.
      b. Set image grandparent <div> to image height + height of text
         below image.
      c. Delete old console log tests.

--- items/show-test/main.js
+++ items/show-test/main.js
@@ -7,20 +7,33 @@
  *
  */
 
+ (function() {
+ 	var picWrapper = document.querySelector(".files__pic"),
+ 		clickNote = document.querySelector(".pic__click-note");
+ 		img = picWrapper.querySelector("img"),
+ 		a = picWrapper.querySelector("a");
+
+ 	picWrapper.style.height = (a.getBoundingClientRect().height + clickNote.getBoundingClientRect().height) + 'px';
+ 	//picWrapper.style.height = window.getComputedStyle(img, null).height;
+ 	console.log(window.getComputedStyle(img, null).height);
+ 	console.log(a.style.height);
+
+ 	window.addEventListener("resize", function() {
+ 		
+ 		picWrapper.style.height = (a.getBoundingClientRect().height + clickNote.getBoundingClientRect().height) + 'px';
+ 	});
+ })();
+
 
 (function() {
 	var sliderEl = document.querySelector('.carousel-item-container');
 	var slideCount = document.getElementsByClassName('carousel-item').length;
 	var sliderManager = new Hammer.Manager(sliderEl);
 	var sliderWrapper 	= document.querySelector(".carousel-wrapper--visible");
-	console.log(sliderWrapper.offsetWidth);
-	console.log(slideCount);
 
 	sliderManager.add(new Hammer.Pan({threshold:0, pointers:0}));
 	sliderManager.on('pan', function(e) {
 		var percent = (100 / slideCount) * e.deltaX / sliderWrapper.offsetWidth;
-		console.log(e.deltaX);
 		sliderEl.style.transform = 'translateX(' + percent + '%)';
-		console.log(sliderEl.style.transform);
 	});
 })();
