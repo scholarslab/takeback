fbd72d8df72a12b977ae34c567ce9ba62dfbc2b5
Katherine <kbd3ax@virginia.edu>
2017-12-07 13:16:30 -0500

    Add AJAX code to items/show JS file

--- items/show-test/main.js
+++ items/show-test/main.js
@@ -7,7 +7,7 @@
  *
  */
 
-// ensure main image is same size as its <a> wrapper
+// main image resizes properly
  (function() {
  	var picWrapper = document.querySelector(".files__pic"),
  		clickNote = document.querySelector(".pic__click-note");
@@ -24,6 +24,87 @@
  		picWrapper.style.height = (a.getBoundingClientRect().height + clickNote.getBoundingClientRect().height) + 'px';
  	});
  })();
+
+// fetch carousel items
+(function() {
+	/*var btn,
+		item,
+		httpRequest;
+
+	btn = document.querySelector('.ajax-btn');
+	item = document.querySelector('.item');
+	
+
+	btn.addEventListener('click', makeRequest);
+
+	
+
+	function makeRequest() { */
+		var httpRequest = new XMLHttpRequest();
+		console.log('test');
+
+		var url = 'http://takeback.scholarslab.org/api/items/' + item.getAttribute('data-id');
+		console.log(url);
+		
+	//	httpRequest.onreadystatechange = populateItem;
+		httpRequest.onload = populateItem;
+		httpRequest.open('GET', url, true);
+		httpRequest.send();
+	//}
+
+	function populateItem() {
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			if (httpRequest.status === 200) {
+				var response = JSON.parse(httpRequest.responseText);
+				console.log(response);
+				/*
+
+				// fetch title
+				for (var i=0; i < response.element_texts.length; i++) {
+					if (response.element_texts[i].element.name === "Title") {
+						var title = response.element_texts[i].text;
+					}
+				}
+
+				// fetch image
+				var files = response.files.url;
+				console.log(files);
+
+				var requestTwo = new XMLHttpRequest();
+				requestTwo.onreadystatechange = myFunction;
+
+				function myFunction() {
+					if (requestTwo.readyState === XMLHttpRequest.DONE) {
+						if (requestTwo.status === 200) {
+							var responseTwo = JSON.parse(requestTwo.responseText);
+							var use = responseTwo[0];
+							var img = use.file_urls.fullsize;
+							item.innerHTML = `<img src="${img}"><br/><div>${title}</div>`;
+						} else {
+							alert('There was an error in processing your request.');
+						}
+					}
+				};
+				requestTwo.open('GET', files, true);
+				requestTwo.send();
+*/
+
+			} else {
+				alert('There was an error in processing your request.');
+			}
+		}
+	}
+})();
+
+
+
+
+
+
+
+
+
+
 
 // carousel gallery
  (function() {
